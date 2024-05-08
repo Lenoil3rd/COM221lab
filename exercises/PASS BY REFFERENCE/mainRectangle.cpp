5cac645f5df2164a4779be03ebe336661a64ef13
Lenoil3rd <bed-com-37-21@unima.ac.mw>
2024-05-08 22:43:34 +0200

    eya

--- exercises/PASS BY REFFERENCE/mainRectangle.cpp
+++ exercises/PASS BY REFFERENCE/mainRectangle.cpp
@@ -9,23 +9,12 @@ int main(){
  cout << "Enter length: ";
  double length;
  cin >> length;
-
-// preventing non-positive values 
- if(length <= 0 ){
-    cerr << "Error: Length must be positive." << endl;
-    return 1;
- }
- 
  rectangle.assignLength(length); // assigning the length to the rectangle object
 
 // get user input for width
  cout << "Enter width: ";
  double width;
  cin >> width;
- if (width <= 0){
-    cerr<< "Error: width must be positive. " << endl;// display error if width is negative
-    return 1;
- }
  rectangle.assignWidth(width);// assigning width to the rectangle object
 
 
